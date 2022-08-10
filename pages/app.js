af45df4534b861fca19b7fd304fa6a7ea7f30330
Igor Stepanov <igorstepanov@hotmail.com>
2022-08-10 15:41:42 +0300

    build(News App): подготовить проект

---
+++ pages/app.js
@@ -0,0 +1,10 @@
+// Custom Http Module
+function customHttp() {}
+
+// Init http module
+const http = customHttp();
+
+//  init selects
+document.addEventListener('DOMContentLoaded', function () {
+  M.AutoInit();
+});
